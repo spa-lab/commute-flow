db7b1dde585367c5953861925052850822bee74a
gisvlasta <vasilis.vlastaras@gmail.com>
2017-01-26 08:42:09 +0000

    classification6

---
+++ js/classification6.js
@@ -0,0 +1,362 @@
+//
+//
+// "Male" "White" "Age 16-24" "Age 25-34" "Age 35-49" "Age 50-64" "Train" "Bus" "Drive Car" "Bike" "Walk" "Higher managerial and administrative" "Higher professional" "Lower professional and higher technical" "Lower managerial and administrative" "Higher supervisory" "Intermediate occupations" "Lower supervisory" "Lower technical" "Semi-routine" "Routine" "Manufacturing" "Construction" "Retail_Rep" "Transport and storage" "Accommodation and food services" "Finance" "Professional, scientific and technical" "Administrative and support services" "Public administration and defence; compulsory social security" "Education" "Human health and social work" "Managers, directors and senior officials" "Professional occupations" "Associate professional and technical occupations" "Administrative and secretarial occupations" "Skilled trades occupations" "Caring, leisure and other service occupations" "Sales and customer service occupations" "Process, plant and machine operatives" "Elementary occupations" "Part-time: 15 or less worked" "Part-time: 16 to 30 hours worked" "Full-time: 31 to 48 hours worked" "Full-time: 49 or more hours worked" "Approx. social grade AB" "Approx. social grade C1" "Approx. social grade C2" "Approx. social grade DE"
+//
+//
+// -0.2898335 -0.5652861 0.9686828 0.0054915 -0.4538302 -0.2901564 -0.0189297 0.8382217 -0.6310537 0.2251787 0.7129810 -0.2821526 -0.5806601 -0.6897176 -0.0081818 0.0553240 -0.0946947 0.2490915 -0.0380303 0.9262800 0.2125665 -0.2480418 -0.1612248 1.1801495 0.0466582 0.6508536 0.0567176 -0.2260128 0.1362883 -0.1332603 -0.2018477 -0.1527481 -0.1707232 -0.7810907 -0.4459161 -0.0758261 -0.0518984 0.1609955 1.5060163 -0.0470359 0.6210566 0.8823794 0.7680842 -0.7551477 -0.4587101 -0.6757754 -0.0836405 0.0983691 0.8167342
+// 0.6921886 -0.2179667 0.0822853 0.0976481 -0.0576619 -0.0329160 -0.2770772 0.2928897 -0.1487155 0.1406652 0.1331504 -0.3063253 -0.6367453 -0.7997461 -0.3110026 -0.2215988 -0.4063652 0.5122560 0.3179224 0.5589849 1.3628863 0.7685182 0.1976581 0.2944021 0.8399924 0.1424404 -0.3265282 -0.4115332 0.3272479 -0.3264130 -0.4612495 -0.3919847 -0.4605616 -0.8942234 -0.5429719 -0.3464534 0.3863276 -0.1571611 -0.0314337 1.3034280 1.1361079 -0.1752512 -0.1604380 0.3242213 -0.1271104 -0.9773783 -0.6692093 0.5131781 1.2242827
+// -0.1383337 -1.1176567 0.0945963 0.7689261 -0.3402549 -0.5478919 0.4941188  0.9284404 -1.1090853 0.6150350 0.2717948 -0.1099024 0.2844357 0.3522773 -0.0484586 -0.0922282 0.0930132 -0.0276900 -0.2942923 -0.1770289 -0.2468847 -0.5057908 -0.3081932 -0.1748507 -0.0645648 0.4210545 0.1265434 0.3608892 0.2169626 0.0057368 0.0487506 0.1198899 -0.1145679 0.3284165 0.2758764 0.1654425 -0.3432391 -0.0459711 0.1055633 -0.3726837 0.0833680 0.0131419 -0.1267229 0.1773345 -0.0876224 0.2310285 0.2599061 -0.4200608 -0.0466512
+// -0.1095386 0.3446663 -0.0540398 -0.0390868 0.1020836 0.0438033 -0.0209371 -0.2074652 0.4025716 -0.1658626 -0.2813606 0.0442326 -0.1238970 0.0151416 0.0071637 0.6050119 0.9862203 -0.1771114 -0.0781881 -0.3003612 -0.3770784 -0.1979887 -0.0495727 -0.1117021 -0.0578071 -0.2558921 0.3970735 0.0337406 0.0397453 1.0803257 -0.2424297 -0.1440021 -0.0168772 -0.3006862 0.6993775 0.8452230 -0.2238220 -0.1956955 0.1235428 -0.2383630 -0.3646593 -0.1952320 -0.1089420 0.3849601 -0.2656536 -0.2120123 0.9244299 -0.2170127 -0.3795964
+// -0.8066910 0.1338176 0.2756893 -0.3428722 -0.0541734 0.2540122 -0.3319610 0.2308739 -0.1093251 0.1632835 0.6909768 -0.2253850 -0.5042127 -0.1618568 -0.1537298 0.0602273 0.1816937 0.2320642 -0.0122877 0.6864039 0.2791192 -0.2053144 0.0167518 -0.1038595 -0.1244135 0.4702802 -0.1720812 -0.2729487 0.0048873 -0.1554605 0.5995690 0.7102548 -0.2975045 -0.2476269 -0.4545134 0.0119751 0.1231673 1.2035224 0.0330400 -0.1103047 0.4353290 0.6052734 0.8394410 -0.7037885 -0.2963621 -0.3896853 -0.2354561 0.5288084 0.4015905
+// -0.8364380 0.2209645 -0.3876986 -0.1547229 0.1092002 0.2868450 -0.3207544 -0.3210237 0.6320914 -0.2436658 -0.3421073 -0.2158200 0.3236253 1.2175987 -0.4532189 -0.1611447 -0.1155475 -0.4046916 -0.4584408 -0.4210402 -0.6141827 -0.5407315 -0.3924519 -0.6531226 -0.5261400 -0.4304750 -0.3713757 -0.3073041 -0.4568249 -0.2099109 0.9686500 0.9382419 -0.4255680 1.3197721 -0.1743819 -0.1963083 -0.5224782 0.2399035 -0.5319696 -0.5739635 -0.6040223 -0.0017715 0.3070976 -0.2816310 0.0442447 0.7225477 -0.0421280 -0.3467375 -0.5860279
+// 0.8118140 0.5647489 -0.1202079 -0.2569240 0.0965638 0.2704365 -0.4000961 -0.5043332 0.7142068 -0.2416887 -0.3652387 0.0557403 -0.2629691 -0.3531495 0.0744140 -0.0905067 -0.2585494 0.2634966 0.9492904 -0.0245353 0.4698080 0.9250227 0.6703313 0.1616322 0.2767232 -0.2586319 -0.3233533 -0.2570401 -0.0710186 -0.2733666 -0.3285085 -0.4442048 0.1123581 -0.4263343 -0.1700432 -0.2077403 1.1180310 -0.3344422 -0.2382526 0.7605151 -0.1046744 -0.3371610 -0.4806472 0.3586286 0.2818674 -0.3540266 -0.2002235 0.7913983 0.0237753
+// 0.5177105 0.4463158 -0.5017661 -0.2271079 0.3715427 0.1453184 -0.3762668 -0.6825343 0.9900446 -0.3863205 -0.5352035 0.7070847 0.8151897 0.1600526 0.6390176 -0.2071901 -0.4403506 -0.3773281 -0.2579754 -0.6915248 -0.5546372 0.3299886 0.0579094 0.0235790 -0.2132989 -0.4366328 -0.0437310 0.3314703 -0.1630259 -0.1360111 -0.2157281 -0.4376039 1.0285703 0.5592542 0.2901612 -0.3308067 -0.3095334 -0.6503084 -0.4117499 -0.3154305 -0.6205056 -0.4113380 -0.5599638 0.2445526 0.5039491 0.9573235 -0.0082092 -0.6272999 -0.7401815
+// 0.3693717 -0.2173867 -0.4305840 0.5077542 0.2015630 -0.4660718 1.8972009 -0.4743537 -1.3264320 -0.0343119 -0.4302274 0.4960191 1.1778595 0.4004408 0.4010174 -0.0358538 -0.1113047 -0.4739118 -0.4897096 -0.8872223 -0.8497859 -0.5995931 -0.2907638 -0.6155724 -0.2019198 -0.3745621 0.9805290 1.1766927 0.0322346 0.1735944 -0.4378472 -0.4991378 0.5652018 0.7461606 0.8060592 0.0928370 -0.6566640 -0.7453708 -0.4848267 -0.6584131 -0.7312058 -0.5575926 -0.7974066 0.4449221 0.5554111 1.1443854 0.0976747 -0.9250956 -0.9181678
+
+
+
+
+
+// "Male" "White" "Age 16-24" "Age 25-34" "Age 35-49" "Age 50-64" "Train" "Bus" "Drive Car" "Bike" "Walk" "Higher managerial and administrative" "Higher professional" "Lower professional and higher technical" "Lower managerial and administrative" "Higher supervisory" "Intermediate occupations" "Lower supervisory" "Lower technical" "Semi-routine" "Routine" "Manufacturing" "Construction" "Retail_Rep" "Transport and storage" "Accommodation and food services" "Finance" "Professional, scientific and technical" "Administrative and support services" "Public administration and defence; compulsory social security" "Education" "Human health and social work" "Managers, directors and senior officials" "Professional occupations" "Associate professional and technical occupations" "Administrative and secretarial occupations" "Skilled trades occupations" "Caring, leisure and other service occupations" "Sales and customer service occupations" "Process, plant and machine operatives" "Elementary occupations" "Part-time: 15 or less worked" "Part-time: 16 to 30 hours worked" "Full-time: 31 to 48 hours worked" "Full-time: 49 or more hours worked" "Approx. social grade AB" "Approx. social grade C1" "Approx. social grade C2" "Approx. social grade DE"
+// -0.2898335 -0.5652861 0.9686828 0.0054915 -0.4538302 -0.2901564 -0.0189297 0.8382217 -0.6310537 0.2251787 0.7129810 -0.2821526 -0.5806601 -0.6897176 -0.0081818 0.0553240 -0.0946947 0.2490915 -0.0380303 0.9262800 0.2125665 -0.2480418 -0.1612248 1.1801495 0.0466582 0.6508536 0.0567176 -0.2260128 0.1362883 -0.1332603 -0.2018477 -0.1527481 -0.1707232 -0.7810907 -0.4459161 -0.0758261 -0.0518984 0.1609955 1.5060163 -0.0470359 0.6210566 0.8823794 0.7680842 -0.7551477 -0.4587101 -0.6757754 -0.0836405 0.0983691 0.8167342
+
+
+
+
+
+
+
+var Diagrams = {
+  // TODO: Update the documentation here.
+
+  createRadialDiagram: function(isSuperGroup) {
+
+    if (isSuperGroup) {
+      this.createSuperGroupsRadialDiagram();
+    }
+    else {
+      this.createGroupsRadialDiagram();
+    }
+
+  },
+
+  createSuperGroupsRadialDiagram: function() {
+
+    var dataDiagram = echarts.init(document.getElementById('dataDiagram'));
+
+    let option = {
+      title : {
+        text: '1. Consumer Services',
+        subtext: 'Test subtext'
+      },
+      tooltip : {
+        trigger: 'axis'
+      },
+      legend: {
+        orient : 'vertical',
+        x : 'right',
+        y : 'bottom',
+        data:['Allocated Budget','Actual Spending']
+      },
+      // toolbox: {
+      //   show : true,
+      //   feature : {
+      //     mark : {show: true},
+      //     dataView : {show: true, readOnly: false},
+      //     restore : {show: true},
+      //     saveAsImage : {show: true}
+      //   }
+      // },
+      polar : [
+        {
+          // indicator : [
+          //   { text: 'Sales', max: 6000},
+          //   { text: 'Administration', max: 16000},
+          //   { text: 'Information Technology', max: 30000},
+          //   { text: 'Customer Support', max: 38000},
+          //   { text: 'Development', max: 52000},
+          //   { text: 'Marketing', max: 25000}
+          // ]
+          indicator : [
+            { text: 'Male' },
+            { text: 'White' },
+            { text: 'Age,16-24' },
+            { text: 'Age,25-34' },
+            { text: 'Age,35-49' },
+            { text: 'Age,50-64' },
+            { text: 'Train' },
+            { text: 'Bus' },
+            { text: 'Drive,Car' },
+            { text: 'Bike' },
+            { text: 'Walk' },
+            { text: 'Higher,managerial,and,administrative' },
+            { text: 'Higher,professional' },
+            { text: 'Lower,professional,and,higher,technical' },
+            { text: 'Lower,managerial,and,administrative' },
+            { text: 'Higher,supervisory' },
+            { text: 'Intermediate,occupations' },
+            { text: 'Lower,supervisory' },
+            { text: 'Lower,technical' },
+            { text: 'Semi-routine' },
+            { text: 'Routine' },
+            { text: 'Manufacturing' },
+            { text: 'Construction' },
+            { text: 'Retail_Rep' },
+            { text: 'Transport,and,storage' },
+            { text: 'Accommodation,and,food,services' },
+            { text: 'Finance' },
+            { text: 'Professional,,scientific,and,technical' },
+            { text: 'Administrative,and,support,services' },
+            { text: 'Public,administration,and,defence;,compulsory,social,security' },
+            { text: 'Education' },
+            { text: 'Human,health,and,social,work' },
+            { text: 'Managers,,directors,and,senior,officials' },
+            { text: 'Professional,occupations' },
+            { text: 'Associate,professional,and,technical,occupations' },
+            { text: 'Administrative,and,secretarial,occupations' },
+            { text: 'Skilled,trades,occupations' },
+            { text: 'Caring,,leisure,and,other,service,occupations' },
+            { text: 'Sales,and,customer,service,occupations' },
+            { text: 'Process,,plant,and,machine,operatives' },
+            { text: 'Elementary,occupations' },
+            { text: 'Part-time:,15,or,less,worked' },
+            { text: 'Part-time:,16,to,30,hours,worked' },
+            { text: 'Full-time:,31,to,48,hours,worked' },
+            { text: 'Full-time:,49,or,more,hours,worked' },
+            { text: 'Approx.,social,grade,AB' },
+            { text: 'Approx.,social,grade,C1' },
+            { text: 'Approx.,social,grade,C2' },
+            { text: 'Approx.,social,grade,DE' }
+          ]
+        }
+      ],
+      //calculable : true,
+      // -0.2898335, -0.5652861, 0.9686828, 0.0054915, -0.4538302, -0.2901564, -0.0189297, 0.8382217, -0.6310537, 0.2251787, 0.7129810, -0.2821526, -0.5806601, -0.6897176, -0.0081818, 0.0553240, -0.0946947, 0.2490915, -0.0380303, 0.9262800, 0.2125665, -0.2480418, -0.1612248, 1.1801495, 0.0466582, 0.6508536, 0.0567176, -0.2260128, 0.1362883, -0.1332603, -0.2018477, -0.1527481, -0.1707232, -0.7810907, -0.4459161, -0.0758261, -0.0518984, 0.1609955, 1.5060163, -0.0470359, 0.6210566, 0.8823794, 0.7680842, -0.7551477, -0.4587101, -0.6757754, -0.0836405, 0.0983691, 0.8167342
+      // series : [
+      //   {
+      //     name: 'Budget vs spending',
+      //     type: 'radar',
+      //     data : [
+      //       {
+      //         value : [4300, 10000, 28000, 35000, 50000, 19000],
+      //         name : 'Allocated Budget'
+      //       },
+      //       {
+      //         value : [5000, 14000, 28000, 31000, 42000, 21000],
+      //         name : 'Actual Spending'
+      //       }
+      //     ]
+      //   }
+      // ]
+      series : [
+        {
+          name: '*** Supergroup data ***',
+          type: 'radar',
+          data : [
+            {
+              value : [-0.2898335, -0.5652861, 0.9686828, 0.0054915, -0.4538302, -0.2901564, -0.0189297, 0.8382217, -0.6310537, 0.2251787, 0.7129810, -0.2821526, -0.5806601, -0.6897176, -0.0081818, 0.0553240, -0.0946947, 0.2490915, -0.0380303, 0.9262800, 0.2125665, -0.2480418, -0.1612248, 1.1801495, 0.0466582, 0.6508536, 0.0567176, -0.2260128, 0.1362883, -0.1332603, -0.2018477, -0.1527481, -0.1707232, -0.7810907, -0.4459161, -0.0758261, -0.0518984, 0.1609955, 1.5060163, -0.0470359, 0.6210566, 0.8823794, 0.7680842, -0.7551477, -0.4587101, -0.6757754, -0.0836405, 0.0983691, 0.8167342],
+              name : '*** Test 1 ***'
+            }
+          ]
+        }
+      ]
+    };
+
+    dataDiagram.setOption(option);
+
+    dataDiagram.on('click', function(params) {
+      alert(params.value); // TODO: Add the data diagram click code.
+    });
+
+    dataDiagram.on('mouseover', function(params) {
+      console.log('mouseover: ' + params.value); // TODO: Add the data diagram mouseover code.
+    });
+
+  },
+
+  createGroupsRadialDiagram: function() {
+
+    var dataDiagram = echarts.init(document.getElementById('dataDiagram'));
+
+    let option = {
+      title : {
+        text: '预算 vs 开销（Budget vs spending）',
+        subtext: '纯属虚构'
+      },
+      tooltip : {
+        trigger: 'axis'
+      },
+      legend: {
+        orient : 'vertical',
+        x : 'right',
+        y : 'bottom',
+        data:['预算分配（Allocated Budget）','实际开销（Actual Spending）']
+      },
+      toolbox: {
+        show : true,
+        feature : {
+          mark : {show: true},
+          dataView : {show: true, readOnly: false},
+          restore : {show: true},
+          saveAsImage : {show: true}
+        }
+      },
+      polar : [
+        {
+          indicator : [
+            { text: '销售（sales）', max: 6000},
+            { text: '管理（Administration）', max: 16000},
+            { text: '信息技术（Information Techology）', max: 30000},
+            { text: '客服（Customer Support）', max: 38000},
+            { text: '研发（Development）', max: 52000},
+            { text: '市场（Marketing）', max: 25000}
+          ]
+        }
+      ],
+      calculable : true,
+      series : [
+        {
+          name: '预算 vs 开销（Budget vs spending）',
+          type: 'radar',
+          data : [
+            {
+              value : [4300, 10000, 28000, 35000, 50000, 19000],
+              name : '预算分配（Allocated Budget）'
+            },
+            {
+              value : [5000, 14000, 28000, 31000, 42000, 21000],
+              name : '实际开销（Actual Spending）'
+            }
+          ]
+        }
+      ]
+    };
+
+    dataDiagram.setOption(option);
+
+    dataDiagram.on('click', function(params) {
+      alert(params.value); // TODO: Add the data diagram click code.
+    });
+
+    dataDiagram.on('mouseover', function(params) {
+      console.log('mouseover: ' + params.value); // TODO: Add the data diagram mouseover code.
+    });
+
+  }
+
+};
+
+
+
+// ================================================================================
+//  View Models.
+
+var toggleClassificationNavigationGroupsViewModel = new Vue({
+
+  /**
+   * The name of the view model.
+   */
+  el: '#toggleClassificationNavigationGroupsVM',
+
+  data: {
+
+    g1: true,
+    g2: true,
+    g3: true,
+    g4: true,
+    g5: true,
+    g6: true,
+    g7: true,
+    g8: true,
+    g9: true
+
+  },
+
+  methods: {
+
+    toggleGroupsVisibility(supergroup) {
+
+      alert(supergroup);
+
+      //this[supergroup] = !this[supergroup];
+
+    }
+
+  }
+
+});
+
+
+var testViewModel = new Vue({
+
+  /**
+   * The name of the view model.
+   */
+  el: '#testVM',
+
+  data: {
+
+    sg1: true,
+    sg2: true,
+    sg3: true,
+    sg4: true,
+    sg5: true,
+    sg6: true,
+    sg7: true,
+    sg8: true,
+    sg9: true
+
+  },
+
+  methods: {
+
+    showSuperGroup(supergroup) {
+
+      alert(supergroup);
+
+      //this[supergroup] = !this[supergroup];
+
+    }
+
+  }
+
+});
+
+
+
+//
+// ================================================================================
+
+
+// ================================================================================
+//  Main Body
+
+
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+$('#nav').affix({
+  offset: {
+    top: $('#nav').offset().top,
+    bottom: $('footer').outerHeight(true) + $('.application').outerHeight(true) + 40
+  }
+});
+
+$("#pop").on("click", function() {
+  $('#imagepreview').attr('src', $('#imageresource').attr('src')); // here asign the image to the modal when the user click the enlarge link
+  $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal
+});
+
+
+Diagrams.createRadialDiagram(true);
+
+//
+// ================================================================================
+
+
+
+
+
+
+
