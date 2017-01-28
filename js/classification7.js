3c92361815b12bb4dbeb049d7d5a6ccac9fe09dd
gisvlasta <vasilis.vlastaras@gmail.com>
2017-01-28 02:06:00 +0000

    test

--- js/classification7.js
+++ js/classification7.js
@@ -34,7 +34,7 @@ var Diagrams = {
   createRadialDiagram: function(isSuperGroup) {
 
     if (isSuperGroup) {
-      this.createSuperGroupsRadialDiagram();
+      this.createSuperGroupsRadialDiagram2();
     }
     else {
       this.createGroupsRadialDiagram();
@@ -317,30 +317,30 @@ var Diagrams = {
             {text : '可用性', max  : 100},
             {text : '功能', max  : 100}
           ],
-          center : ['50%',200],
+          center : ['25%',200],
           radius : 80
         },
-        // {
-        //   indicator : [
-        //     {text : '外观', max  : 100},
-        //     {text : '拍照', max  : 100},
-        //     {text : '系统', max  : 100},
-        //     {text : '性能', max  : 100},
-        //     {text : '屏幕', max  : 100}
-        //   ],
-        //   radius : 80
-        // },
-        // {
-        //   indicator : (function (){
-        //     var res = [];
-        //     for (var i = 1; i <= 12; i++) {
-        //       res.push({text:i+'月',max:100});
-        //     }
-        //     return res;
-        //   })(),
-        //   center : ['75%', 200],
-        //   radius : 80
-        // }
+        {
+          indicator : [
+            {text : '外观', max  : 100},
+            {text : '拍照', max  : 100},
+            {text : '系统', max  : 100},
+            {text : '性能', max  : 100},
+            {text : '屏幕', max  : 100}
+          ],
+          radius : 80
+        },
+        {
+          indicator : (function (){
+            var res = [];
+            for (var i = 1; i <= 12; i++) {
+              res.push({text:i+'月',max:100});
+            }
+            return res;
+          })(),
+          center : ['75%', 200],
+          radius : 80
+        }
       ],
       series : [
         {
@@ -356,35 +356,35 @@ var Diagrams = {
             }
           ]
         },
-        // {
-        //   type: 'radar',
-        //   polarIndex : 1,
-        //   data : [
-        //     {
-        //       value : [85, 90, 90, 95, 95],
-        //       name : '某主食手机'
-        //     },
-        //     {
-        //       value : [95, 80, 95, 90, 93],
-        //       name : '某水果手机'
-        //     }
-        //   ]
-        // },
-        // {
-        //   type: 'radar',
-        //   polarIndex : 2,
-        //   itemStyle: {normal: {areaStyle: {type: 'default'}}},
-        //   data : [
-        //     {
-        //       name : '降水量',
-        //       value : [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 75.6, 82.2, 48.7, 18.8, 6.0, 2.3],
-        //     },
-        //     {
-        //       name:'蒸发量',
-        //       value:[2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 35.6, 62.2, 32.6, 20.0, 6.4, 3.3]
-        //     }
-        //   ]
-        // }
+        {
+          type: 'radar',
+          polarIndex : 1,
+          data : [
+            {
+              value : [85, 90, 90, 95, 95],
+              name : '某主食手机'
+            },
+            {
+              value : [95, 80, 95, 90, 93],
+              name : '某水果手机'
+            }
+          ]
+        },
+        {
+          type: 'radar',
+          polarIndex : 2,
+          itemStyle: {normal: {areaStyle: {type: 'default'}}},
+          data : [
+            {
+              name : '降水量',
+              value : [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 75.6, 82.2, 48.7, 18.8, 6.0, 2.3],
+            },
+            {
+              name:'蒸发量',
+              value:[2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 35.6, 62.2, 32.6, 20.0, 6.4, 3.3]
+            }
+          ]
+        }
       ]
     };
 
